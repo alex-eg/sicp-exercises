84cfbcae5227e7ca7c32fc9dab33ee4ec48a242a
taptap <alexey.e.egorov@gmail.com>
2014-07-04 19:21:28 +0400

    formatted block structure

--- Chapter1.rkt
+++ Chapter1.rkt
@@ -4,19 +4,16 @@
 (define (square x)
   (* x x))
 
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (sqrt-iter (improve guess x)
-                 x)))
-
-(define (improve guess x)
-  (/ (+ guess (/ x guess)) 2))
-
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
-
 (define (sqrt x)
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess)
+                   x)))
+  (define (improve guess)
+    (/ (+ guess (/ x guess)) 2))
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
   (sqrt-iter 1.0 x))
 
 ;;; 1.8 cubic interpolation
@@ -24,17 +21,14 @@
 (define (cube x)
   (* x x x))
 
-(define (root3-iter guess x)
-  (if (good-enough-cube? guess x)
-      guess
-      (root3-iter (root3-improve guess x)
-                  x)))
-
-(define (good-enough-cube? guess x)
-  (< (abs (- (cube guess) x)) 0.001))
-
-(define (root3-improve guess x)
-  (/ (+ (* 2 guess) (/ x (square guess))) 3))
-
 (define (root3 x)
+  (define (root3-iter guess x)
+    (if (good-enough-cube? guess x)
+        guess
+        (root3-iter (root3-improve guess x)
+                    x)))
+  (define (good-enough-cube? guess x)
+    (< (abs (- (cube guess) x)) 0.001))
+  (define (root3-improve guess x)
+    (/ (+ (* 2 guess) (/ x (square guess))) 3))
   (root3-iter 1.0 x))
