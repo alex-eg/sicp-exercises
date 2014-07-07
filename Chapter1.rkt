7261e1bd7a33cd2addeada4aff8fec195ee0d9aa
alex_e <alexey.e.egorov@gmail.com>
2014-07-07 15:35:01 +0400

    add logarithmic fibonatti computatuion algorithm

--- Chapter1.rkt
+++ Chapter1.rkt
@@ -69,3 +69,20 @@
           (else (*-iter a (- b 1) (+ acc a)))))
   (*-iter a b 0))
                      
+;;; 1.19 logarithmic Fibonatti numbers
+
+(define (fib n)
+  (define (fib-iter a b p q count)
+    (cond ((= count 0) b)
+          ((even? count)
+           (fib-iter a
+                     b
+                     (+ (* q q) (* p p))
+                     (+ (* q q) (* 2 q p))
+                     (/ count 2)))
+          (else (fib-iter (+ (* b q) (* a q) (* a p))
+                          (+ (* b p) (* a q))
+                          p
+                          q
+                          (- count 1)))))
+  (fib-iter 1 0 0 1 n))
