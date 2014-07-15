e0dd694049f3e9dab3a94f7b445d70b5aad4518a
alex_e <alexey.e.egorov@gmail.com>
2014-07-15 15:49:49 +0400

    add iterative sum

--- Chapter1.rkt
+++ Chapter1.rkt
@@ -94,6 +94,13 @@
       0
       (+ (term a) (sum (next a) b term next))))
 
+(define (sum-iterative a b term next)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
 ;; simple
 (define (integral f a b dx)
   (define (add-dx x) (+ x dx))
