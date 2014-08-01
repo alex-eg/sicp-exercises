7b39edc0c100825310b19e0d3bbf5632ac12f20c
alex_e <alexey.e.egorov@gmail.com>
2014-08-01 17:00:21 +0400

    add rational number abstraction

--- Chapter 2.rkt
+++ Chapter 2.rkt
@@ -1,3 +1,45 @@
 #lang racket
 
 ;;; Chapter 2
+
+;;; Rational numbers data abstraction
+
+(define (make-rat n d)
+  (let ((g (gcd n d))
+        (sign (/ (* n d) (abs (* n d)))))
+    (cons (* sign (abs (/ n g))) (abs (/ d g)))))
+
+(define (numer num)
+  (car num))
+
+(define (denom num)
+  (cdr num))
+
+(define (print-rat num)
+  (newline)
+  (printf "~s/~s"
+          (numer num)
+          (denom num)))
+          
+
+(define (add-rat r1 r2)
+  (make-rat (+ (* (numer r1) (denom r2))
+               (* (denom r1) (numer r2)))
+            (* (denom r1) (denom r2))))
+
+(define (sub-rat r1 r2)
+  (make-rat (- (* (numer r1) (denom r2))
+               (* (denom r1) (numer r2)))
+            (* (denom r1) (denom r2))))
+
+(define (mul-rat r1 r2)
+  (make-rat (* (numer r1) (numer r2))
+            (* (denom r1) (denom r2))))
+
+(define (div-rat r1 r2)
+  (make-rat (* (numer r1) (denom r2))
+            (* (denom r1) (numer r2))))
+
+(define (equal-rat? r1 r2)
+  (= (* (numer r1) (denom r2))
+     (* (denom r2) (numer r2))))
