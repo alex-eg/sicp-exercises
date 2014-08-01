c03443e613a2588d08e61d7ec0df8b371c5598ad
alex_e <alexey.e.egorov@gmail.com>
2014-08-01 14:53:17 +0400

    fix sqrt function

--- Chapter1.rkt
+++ Chapter1.rkt
@@ -8,13 +8,12 @@
   (define (sqrt-iter guess)
     (if (good-enough? guess)
         guess
-        (sqrt-iter (improve guess)
-                   x)))
+        (sqrt-iter (improve guess))))
   (define (improve guess)
     (/ (+ guess (/ x guess)) 2))
   (define (good-enough? guess)
     (< (abs (- (square guess) x)) 0.001))
-  (sqrt-iter 1.0 x))
+  (sqrt-iter 1.0))
 
 ;;; 1.8 cubic interpolation
 
