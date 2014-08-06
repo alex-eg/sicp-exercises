237666e9d72a4c9fa4d688a23684338a640d9343
taptap <alexey.e.egorov@gmail.com>
2014-08-06 08:28:59 +0400

    add 2.2 2.3 2.4 2.6

--- Chapter 2.rkt
+++ Chapter 2.rkt
@@ -43,3 +43,77 @@
 (define (equal-rat? r1 r2)
   (= (* (numer r1) (denom r2))
      (* (denom r2) (numer r2))))
+
+
+;;; 2.2 point data
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (midpoint-segment segment)
+  (make-point (/ (+ (x-point (start-segment segment))
+                    (x-point (end-segment segment)))
+                 2)
+              (/ (+ (y-point (start-segment segment))
+                    (y-point (end-segment segment)))
+                 2)))
+
+;;; 2.3 rectangles
+
+(define (make-rectangle tl br)
+  (cons tl br))
+
+(define (rect-top-left rect)
+  (car rect))
+
+(define (rect-bottom-right rect)
+  (cdr rect))
+
+
+;;; 2.4 Functional pairs
+
+(define (ch-cons x y)
+  (λ (m) (m x y)))
+
+(define (ch-car z)
+  (z (λ (p q) p)))
+
+(define (ch-cdr z)
+  (z (λ (p q) q)))
+
+;;; 2.6 Church numbers
+
+(define (zero)
+  (λ (f) (λ (x) x)))
+
+(define (add-1 n)
+  (λ (f) (λ (x) (f ((n f) x)))))
+
+(define (one)
+  (λ (f) (λ (x) (f x))))
+
+(define (two)
+  (λ (f) (λ (x) (f (f x)))))
